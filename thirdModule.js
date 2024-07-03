07049e3306ebbe73f983c3b9c3d367b1d1a2605d
krok-code <maks.smolinskyi@gmail.com>
2024-07-03 19:14:27 +0300

    finish third module

---
+++ thirdModule.js
@@ -0,0 +1,14 @@
+// const adTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   removePotion(potionName) {},
+// };
+
+// const bookShelf = {
+//   books: ["The last kingdom", "Haze", "The guardian of dreams"],
+//   updateBook(oldName, newName) {
+//     const newBooks = this.books.map((book) => (book === oldName ? newName : book));
+//     this.books = [...newBooks];
+//   },
+// };
+
+// console.log(bookShelf.updateBook("Haze", "Dungeon chronicles"));
